13af76d0a9a8883cf8a151f0ad0e446343305564
Michael Z <mzhan1989@gmail.com>
2020-08-13 21:39:30 -0400

    basic scraping info

--- src/scraper.js
+++ src/scraper.js
@@ -1,8 +1,9 @@
     const cheerio = require('cheerio');
     const puppeteer = require('puppeteer');
 
-    const url = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=00df01ca43e3426d91487697c76755b8&App=TREB#C4835471';
-
+    //const url = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=00df01ca43e3426d91487697c76755b8&App=TREB#C4835471'; //multilist condo
+    const url = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=a73e6e2779a54a9d8112dce842541694&App=TREB' //single listing house
+    
     puppeteer
       .launch()
       .then(browser => browser.newPage())
@@ -13,8 +14,6 @@
       })
       .then(html => {
         const $ = cheerio.load(html);
-        //console.log(html);
-        const items = [];
 
         //#C4835471 > span
 
@@ -36,24 +35,36 @@
 
         const urlElems = $('div.formitem.form.viewform')
           for (let i = 0; i < urlElems.length; i++) {
-            const address = $(urlElems[i]).find('div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div > div > span:nth-child(1) > span').text()
-
-            var price = $(urlElems[i]).find('label').filter(function() {
+            const listingData = {
+            address : $(urlElems[i]).find('div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div > div > span:nth-child(1) > span').text(),
+            price : $(urlElems[i]).find('label').filter(function() {
               return $(this).text().trim() === 'List:';
-            }).next().text();
-
-            const taxes = $(urlElems[i]).find('label').filter(function() {
+            }).next().text(),
+            taxes : $(urlElems[i]).find('label').filter(function() {
               return $(this).text().trim() === 'Taxes:';
-            }).next().text();
+            }).next().text(),
+            forsale : $(urlElems[i]).find('label').filter(function() {
+              return $(this).text().trim() === 'For:';
+            }).next().text(),
+            MLS : $(urlElems[i]).find('label').filter(function() {
+              return $(this).text().trim() === 'MLS#:';
+            }).next().text(),
+            style : $(urlElems[i]).find('div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(5) > div.formitem.formgroup.horizontal > div:nth-child(1) > span:nth-child(1) > span').text(),
+            acre : $(urlElems[i]).find('div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(5) > div.formitem.formgroup.vertical > span:nth-child(4) > span').text(),
+            Rms : $(urlElems[i]).find('label').filter(function() {
+              return $(this).text().trim() === 'Rms:';
+            }).next().text(),
+            Bedrooms : $(urlElems[i]).find('label').filter(function() {
+              return $(this).text().trim() === 'Bedrooms:';
+            }).next().text(),
+            Washrooms : $(urlElems[i]).find('label').filter(function() {
+              return $(this).text().trim() === 'Washrooms:';
+            }).next().text()
+          }
 
-            
             const images = JSON.parse($(urlElems[i]).find('img').attr('data-multi-photos'))
             
-            console.log(address, price,taxes, images)
-          
+            console.log(listingData, images)
         }
-        
-        
-
       })
       .catch(console.error);
